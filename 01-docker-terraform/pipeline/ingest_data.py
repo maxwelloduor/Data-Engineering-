4b6e17e774499ddba51b2adc40659a7f122f1005
maxwelloduor <137492526+maxwelloduor@users.noreply.github.com>
2026-04-09 16:12:45 +0000

    refactor: optimize ingestion script for container execution

--- 01-docker-terraform/pipeline/ingest_data.py
+++ 01-docker-terraform/pipeline/ingest_data.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import click
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
@@ -72,17 +73,27 @@ def ingest_data(
 
     print(f'done ingesting to {target_table}')
 
-def main():
-    pg_user = 'root'
-    pg_pass = 'root'
-    pg_host = 'localhost'
-    pg_port = '5432'
-    pg_db = 'ny_taxi'
-    year = 2021
-    month = 1
-    chunksize = 100000
-    target_table = 'yellow_taxi_data'
-
+@click.command()
+@click.option("--pg-user", default="root", show_default=True, help="Postgres username")
+@click.option("--pg-pass", default="root", show_default=True, help="Postgres password")
+@click.option("--pg-host", default="localhost", show_default=True, help="Postgres host")
+@click.option("--pg-port", default="5432", show_default=True, help="Postgres port")
+@click.option("--pg-db", default="ny_taxi", show_default=True, help="Postgres database name")
+@click.option("--year", default=2021, show_default=True, type=int, help="Year of the taxi data file")
+@click.option("--month", default=1, show_default=True, type=int, help="Month of the taxi data file")
+@click.option("--chunksize", default=100000, show_default=True, type=int, help="CSV read chunk size")
+@click.option("--target-table", default="yellow_taxi_data", show_default=True, help="Target PostgreSQL table")
+def main(
+    pg_user: str,
+    pg_pass: str,
+    pg_host: str,
+    pg_port: str,
+    pg_db: str,
+    year: int,
+    month: int,
+    chunksize: int,
+    target_table: str,
+):
     engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
     url_prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow'
 
