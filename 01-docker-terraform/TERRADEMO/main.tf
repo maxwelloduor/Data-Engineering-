68c9e0d9fdd018b98d57e36de6af727a466bab10
maxwelloduor <137492526+maxwelloduor@users.noreply.github.com>
2026-04-10 18:53:55 +0000

    Finalize Terraform configuration with variables for GCS and BigQuery

--- 01-docker-terraform/TERRADEMO/main.tf
+++ 01-docker-terraform/TERRADEMO/main.tf
@@ -2,45 +2,37 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "4.51.0"
+      version = "5.6.0"
     }
   }
 }
 
 provider "google" {
-  project = "terraform-demo-452507"
-  region  = "us-central1"
+  credentials = file(var.credentials)
+  project     = var.project
+  region      = var.region
 }
 
 
+resource "google_storage_bucket" "demo-bucket" {
+  name          = var.gcs_bucket_name
+  location      = var.location
+  force_destroy = true
 
-resource "google_storage_bucket" "data-lake-bucket" {
-  name          = "terraform-demo-452507-demo-bucket"
-  location      = "US"
-
-  # Optional, but recommended settings:
-  storage_class = "STANDARD"
-  uniform_bucket_level_access = true
-
-  versioning {
-    enabled     = true
-  }
 
   lifecycle_rule {
-    action {
-      type = "Delete"
-    }
     condition {
-      age = 1  // days
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
     }
   }
-
-  force_destroy = true
 }
 
 
-resource "google_bigquery_dataset" "dataset" {
-  dataset_id = "<The Dataset Name You Want to Use>"
-  project    = "terraform-demo-452507"
-  location   = "US"
+
+resource "google_bigquery_dataset" "demo_dataset" {
+  dataset_id = var.bq_dataset_name
+  location   = var.location
 }
